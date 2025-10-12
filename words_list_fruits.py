fdacc754f1309b079d063188e3082ea51c38c399
GRINDER -_- <jyotisharma1k@gmail.com>
2025-10-12 19:18:23 +0530

    Update words_list_fruits.py

--- words_list_fruits.py
+++ words_list_fruits.py
@@ -7,12 +7,12 @@ words = [
     "jackfruit", "fig", "date", "coconut", "melon", "muskmelon", "starfruit", "passionfruit",
     "cranberry", "tangerine", "clementine", "nectarine", "persimmon", "mulberry", "boysenberry",
     "gooseberry", "currant", "soursop", "durian", "longan", "rambutan", "sapodilla", "jamun",
-    "bael", "custard apple", "breadfruit", "olive", "kumquat", "plantain", "bilberry", "ackee",
-    "miracle fruit", "tamarind", "santol", "mangosteen", "langsat", "salak", "rose apple",
-    "jambu", "black sapote", "white sapote", "feijoa", "cactus pear", "prickly pear", "surinam cherry",
+    "bael", "custardapple", "breadfruit", "olive", "kumquat", "plantain", "bilberry", "ackee",
+    "miraclefruit", "tamarind", "santol", "mangosteen", "langsat", "salak", "roseapple",
+    "jambu", "blacksapote", "whitesapote", "feijoa", "cactuspear", "pricklypear", "surinamcherry",
     "chokeberry", "elderberry", "barberry", "medlar", "quince", "ugli fruit", "yuzu", "pomelo",
-    "blood orange", "mandarin", "calamansi", "honeydew", "canary melon", "galia melon", "sugar apple",
-    "marang", "lucuma", "jabuticaba", "buddha’s hand", "loquat", "imbe", "bignay", "maqui berry",
-    "sea buckthorn", "acerola", "sloe", "rowan berry", "naranjilla", "tangelo", "grenadilla",
+    "blood orange", "mandarin", "calamansi", "honeydew", "canary melon", "galiamelon", "sugarapple",
+    "marang", "lucuma", "jabuticaba", "buddhashand", "loquat", "imbe", "bignay", "maquiberry",
+    "seabuckthorn", "acerola", "sloe", "rowanberry", "naranjilla", "tangelo", "grenadilla",
     "tamarillo", "cherimoya", "white currant", "black currant", "red currant", "carambola"
 ]
