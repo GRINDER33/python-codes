1422ce062bde564e3601ad8b536b4bd7ff026d2f
GRINDER -_- <jyotisharma1k@gmail.com>
2025-10-21 01:03:12 +0530

    Update # Practice program 13 countdown timer.py

--- # Practice program 13 countdown timer.py
+++ # Practice program 13 countdown timer.py
@@ -1,11 +1,52 @@
 # Practice program 13 countdown timer
 
 import time 
+import winsound
+
+def slow_print(text, delay=0.02):
+    for letter in text:
+        print(letter, end="", flush=True)
+        time.sleep(delay)
+
 
 while True:
     try:
-        my_time = int(input("Enter the time in seconds: "))
+        ## Hours
+        slow_print("Enter hours: ")
+        time_hour_input = input()
+        if time_hour_input.strip() == "":
+            time_hour = 0
+        else:
+            time_hour = int(time_hour_input)
+            if time_hour < 0:
+                raise ValueError("Negative value not allowed")
 
+        ## Minutes
+        slow_print("Enter minutes: ")
+        time_minutes_input = input()
+        if time_minutes_input.strip() == "":
+            time_minutes = 0
+        else:  
+            time_minutes = int(time_minutes_input)
+            if time_minutes < 0:
+                raise ValueError("Negative value not allowed")
+
+        ## Seconds
+        slow_print("Enter seconds: ")
+        time_seconds_input = input()
+        if time_seconds_input.strip() == "":
+            time_seconds = 0
+        else:  
+            time_seconds = int(time_seconds_input)
+            if time_seconds < 0:
+                raise ValueError("Negative value not allowed")
+
+        my_time = time_hour * 3600 + time_minutes * 60 + time_seconds
+
+        print()
+        slow_print(f"Timer set for {time_hour} hours, {time_minutes} minutes and {time_seconds} seconds.")
+        print()
+   
         for x in range(my_time , 0 , -1):
             seconds = x % 60
             minutes = int(x / 60) % 60 
@@ -13,6 +54,11 @@ while True:
             print(f"{hours:02}:{minutes:02}:{seconds:02}" ,end="\r")     # \r is a carriage return character   overwrites the previous print
             time.sleep(1)
         print("Times Up!")
+
+        stop_flag = False
+
+        sound = winsound.PlaySound("yo_phone_linging.wav", winsound.SND_FILENAME)
+        
         exit_fnc = input("Press q to exit or enter to stay: ")
         if exit_fnc.lower().startswith("q"):
             print("Exiting...")
